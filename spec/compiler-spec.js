07b3bf9e2fada17e7607178d03255aea59cda20e
torii_y <torii_y@worksap.co.jp>
2014-12-03 14:36:33 +0900

    rough

--- spec/compiler-spec.js
+++ spec/compiler-spec.js
@@ -4,11 +4,16 @@ var Compiler = require('../lib/compiler.js');
 describe('converter', function() {
 
   it('can compile import', function() {
-    var s = new Compiler(esprima).compile('import foo from "bar.js"');
+    var s = new Compiler(esprima).compile('import Bar from "bar.js"', 'name.space');
+    console.log(s);
     expect(s.indexOf('goog.require') >= 0).toBe(true);
+    expect(s.indexOf('name.space.Bar') >= 0).toBe(true);
+    // expect(s.indexOf('goog.require') >= 0).toBe(true);
   });
   it('can compile export', function() {
-    var s = new Compiler(esprima).compile('export default "foo"');
+    var s = new Compiler(esprima).compile('export default "Bar"', 'name.space');
+    console.log(s);
     expect(s.indexOf('goog.provide') >= 0).toBe(true);
+    expect(s.indexOf('name.space.Bar') >= 0).toBe(true);
   });
 });
