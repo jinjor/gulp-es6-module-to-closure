49bb39642db2ed6522f01a291cdc3f7e2d057a77
torii_y <torii_y@worksap.co.jp>
2014-12-03 15:10:48 +0900

    rough

--- spec/compiler-spec.js
+++ spec/compiler-spec.js
@@ -1,19 +1,29 @@
 var esprima = require('esprima-fb');
 var Compiler = require('../lib/compiler.js');
 
+var c = new Compiler(esprima)
+
 describe('converter', function() {
 
   it('can compile import', function() {
-    var s = new Compiler(esprima).compile('import Bar from "bar.js"', 'name.space');
+    var s = c.compile('import {foo,bar} from "foo.js";', 'name.space');
+    console.log(s);
+    expect(s.indexOf('goog.require') >= 0).toBe(true);
+    expect(s.indexOf('name.space.foo') >= 0).toBe(true);
+    expect(s.indexOf('name.space.bar') >= 0).toBe(true);
+  });
+  it('can compile default import', function() {
+    var s = c.compile('import Bar from "bar.js";import Foo from "foo.js";', 'name.space');
     console.log(s);
     expect(s.indexOf('goog.require') >= 0).toBe(true);
     expect(s.indexOf('name.space.Bar') >= 0).toBe(true);
-    // expect(s.indexOf('goog.require') >= 0).toBe(true);
   });
-  it('can compile export', function() {
-    var s = new Compiler(esprima).compile('export default "Bar"', 'name.space');
+
+  it('can compile default export', function() {
+    var s = c.compile('export default "Foo";export default "Bar";', 'name.space');
     console.log(s);
     expect(s.indexOf('goog.provide') >= 0).toBe(true);
+    expect(s.indexOf('name.space.Foo') >= 0).toBe(true);
     expect(s.indexOf('name.space.Bar') >= 0).toBe(true);
   });
 });
