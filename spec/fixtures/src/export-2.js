c8978a3a61832b5f19c5eb61957a3f2ed0b68b52
Yosuke Torii <jinjorweb@gmail.com>
2014-12-05 18:37:08 +0900

    close #10 Export named function

---
+++ spec/fixtures/src/export-2.js
@@ -0,0 +1,3 @@
+export function f(a) {
+  console.log(a)
+};
--- spec/simple-compiler-spec.js
+++ spec/simple-compiler-spec.js
@@ -18,6 +18,16 @@ describe('compiler', function() {
     expect(s.indexOf('goog.require') >= 0).toBe(true);
     expect(s.indexOf('name.space.foo.bar.Baz') >= 0).toBe(true);//TODO add more tests
   });
+
+  it('can compile import 3', function() {
+    var c = new Compiler();
+    var s = c.compile('import {foo,bar} from "foo.js";', 'sample.js', 'name.space');
+    // console.log(s);
+    expect(s.indexOf('goog.require') >= 0).toBe(true);
+    expect(s.indexOf('name.space.foo') >= 0).toBe(true);
+    expect(s.indexOf('name.space.bar') >= 0).toBe(true);
+  });
+
   it('can compile default import', function() {
     var c = new Compiler();
     var s = c.compile('import Bar from "bar.js";import Foo from "foo.js";', 'sample.js', 'name.space');
@@ -27,7 +37,7 @@ describe('compiler', function() {
     expect(s.indexOf('name.space.Foo') >= 0).toBe(true);
   });
 
-  it('can compile export', function() {
+  it('can compile export (VariableDeclaration)', function() {
     var c = new Compiler();
     var s = c.compile('export var foo = "Foo";', 'sample.js', 'name.space');
     // console.log(s);
@@ -35,7 +45,15 @@ describe('compiler', function() {
     expect(s.indexOf('name.space.foo') >= 0).toBe(true);
     expect(s.indexOf('Foo') >= 0).toBe(true);
   });
-  it('can compile export 2', function() {
+  it('can compile export (FunctionDeclaration)', function() {
+    var c = new Compiler();
+    var s = c.compile('export default function f(a) { console.log(a); };', 'sample.js', 'name.space');
+    // console.log(s);
+    expect(s.indexOf('goog.provide') >= 0).toBe(true);
+    expect(s.indexOf('name.space.f') >= 0).toBe(true);
+    expect(s.indexOf('console.log') >= 0).toBe(true);
+  });
+  it('can compile export 3', function() {
     var c = new Compiler();
     var s = c.compile('export { foo, bar };', 'sample.js', 'name.space');
     // console.log(s);
