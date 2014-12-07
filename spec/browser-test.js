d899db29a6b3c9f321e20715d41eebc1c9aad4fb
Yosuke Torii <jinjorweb@gmail.com>
2014-12-08 00:00:24 +0900

    test passed

--- spec/browser-test.js
+++ spec/browser-test.js
@@ -1,15 +1,15 @@
-goog.require('com.xxx.foo');
-goog.require('com.xxx.bar');
-goog.require('com.xxx.ns.foo');
-goog.require('com.xxx.ns.bar');
+goog.require('com.xxx.export.foo');
+goog.require('com.xxx.import.bar');
+goog.require('com.xxx.ns.export.foo');
+goog.require('com.xxx.ns.import.bar');
 
 describe('index.js', function() {
 
   it('create closure-formatted .js files', function() {
-    expect(com.xxx.foo).toBe('FOO');
-    expect(com.xxx.bar).toBe('FOOBAR');
-    expect(com.xxx.ns.foo).toBe('FOO');
-    expect(com.xxx.ns.bar).toBe('FOOBAR');
+    expect(com.xxx.export.foo).toBe('FOO');
+    expect(com.xxx.import.bar).toBe('FOOBAR');
+    expect(com.xxx.ns.export.foo).toBe('FOO');
+    expect(com.xxx.ns.import.bar).toBe('FOOBAR');
   });
 
 });
