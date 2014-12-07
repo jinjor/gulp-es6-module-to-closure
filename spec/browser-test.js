5ce8c71b12c8aaadde589de6345bf9f0c78cf965
Yosuke Torii <jinjorweb@gmail.com>
2014-12-08 03:30:18 +0900

    pass test

--- spec/browser-test.js
+++ spec/browser-test.js
@@ -2,6 +2,7 @@ goog.require('com.xxx.export.foo');
 goog.require('com.xxx.import.bar');
 goog.require('com.xxx.export2.foo');
 goog.require('com.xxx.import3.bar');
+goog.require('com.xxx.import4.bar');
 goog.require('com.xxx.ns.export.foo');
 goog.require('com.xxx.ns.import.bar');
 goog.require('com.xxx.importdefault.bar');
@@ -17,6 +18,7 @@ describe('index.js', function() {
     expect(com.xxx.importdefault.bar).toBe('FOOBAR');
     expect(typeof com.xxx.export2.foo).toBe('function');// for #18
     expect(com.xxx.import3.bar).toBe(3);
+    expect(com.xxx.import4.bar).toBe(3.5);
   });
 
 });
