6f938e8ec34e66f78ba274c85480086dee875fd4
Yosuke Torii <jinjorweb@gmail.com>
2014-12-07 23:50:31 +0900

    fix namespace

--- spec/simple-compiler-spec.js
+++ spec/simple-compiler-spec.js
@@ -8,23 +8,23 @@ describe('compiler', function() {
     var s = c.compile('import {foo,bar} from "foo.js";', 'sample.js', 'name.space');
     // console.log(s);
     expect(s.indexOf('goog.require') >= 0).toBe(true);
-    expect(s.indexOf('name.space.foo') >= 0).toBe(true);
-    expect(s.indexOf('name.space.bar') >= 0).toBe(true);
+    expect(s.indexOf('name.space.foo.foo') >= 0).toBe(true);
+    expect(s.indexOf('name.space.foo.bar') >= 0).toBe(true);
   });
   it('can compile import(no name space)', function() {
     var c = new Compiler();
     var s = c.compile('import {foo,bar} from "foo.js";', 'sample.js');
     // console.log(s);
     expect(s.indexOf('goog.require') >= 0).toBe(true);
-    expect(s.indexOf(' foo') >= 0).toBe(true);
-    expect(s.indexOf(' bar') >= 0).toBe(true);
+    expect(s.indexOf(' foo.foo') >= 0).toBe(true);
+    expect(s.indexOf(' foo.bar') >= 0).toBe(true);
   });
   it('can compile import 2', function() {
     var c = new Compiler();
     var s = c.compile('import {Baz} from "./foo/bar/baz.js";', 'sample.js', 'name.space');
     // console.log(s);
     expect(s.indexOf('goog.require') >= 0).toBe(true);
-    expect(s.indexOf('name.space.foo.bar.Baz') >= 0).toBe(true);//TODO add more tests
+    expect(s.indexOf('name.space.foo.bar.baz.Baz') >= 0).toBe(true);//TODO add more tests
   });
 
   it('can compile import multi', function() {
@@ -32,8 +32,8 @@ describe('compiler', function() {
     var s = c.compile('import {foo,bar} from "foo.js";', 'sample.js', 'name.space');
     // console.log(s);
     expect(s.indexOf('goog.require') >= 0).toBe(true);
-    expect(s.indexOf('name.space.foo') >= 0).toBe(true);
-    expect(s.indexOf('name.space.bar') >= 0).toBe(true);
+    expect(s.indexOf('name.space.foo.foo') >= 0).toBe(true);
+    expect(s.indexOf('name.space.foo.bar') >= 0).toBe(true);
   });
 
   it('can compile import A as B', function() {
@@ -41,17 +41,17 @@ describe('compiler', function() {
     var s = c.compile('import {foo as foofoo} from "foo.js";', 'sample.js', 'name.space');
     // console.log(s);
     expect(s.indexOf('goog.require') >= 0).toBe(true);
-    expect(s.indexOf('name.space.foo') >= 0).toBe(true);
-    expect(s.indexOf('foofoo = name.space.foo') >= 0).toBe(true);
+    expect(s.indexOf('name.space.foo.foo') >= 0).toBe(true);
+    expect(s.indexOf('foofoo = name.space.foo.foo') >= 0).toBe(true);
   });
 
   it('can compile import 3', function() {
     var c = new Compiler();
-    var s = c.compile('import {Bar} from "bar.js";import Foo from "foo.js";', 'sample.js', 'name.space');
+    var s = c.compile('import {Bar} from "bar.js";import {Foo} from "foo.js";', 'sample.js', 'name.space');
     // console.log(s);
     expect(s.indexOf('goog.require') >= 0).toBe(true);
-    expect(s.indexOf('name.space.Bar') >= 0).toBe(true);
-    expect(s.indexOf('name.space.Foo') >= 0).toBe(true);
+    expect(s.indexOf('name.space.bar.Bar') >= 0).toBe(true);
+    expect(s.indexOf('name.space.foo.Foo') >= 0).toBe(true);
   });
 
   it('can compile export (VariableDeclaration:var)', function() {
@@ -59,7 +59,7 @@ describe('compiler', function() {
     var s = c.compile('export var foo = "Foo";', 'sample.js', 'name.space');
     // console.log(s);
     expect(s.indexOf('goog.provide') >= 0).toBe(true);
-    expect(s.indexOf('name.space.foo') >= 0).toBe(true);
+    expect(s.indexOf('name.space.sample.foo') >= 0).toBe(true);
     expect(s.indexOf('Foo') >= 0).toBe(true);
   });
   it('can compile export (VariableDeclaration:let)', function() {
@@ -67,7 +67,7 @@ describe('compiler', function() {
     var s = c.compile('export let foo = "Foo";', 'sample.js', 'name.space');
     // console.log(s);
     expect(s.indexOf('goog.provide') >= 0).toBe(true);
-    expect(s.indexOf('name.space.foo') >= 0).toBe(true);
+    expect(s.indexOf('name.space.sample.foo') >= 0).toBe(true);
     expect(s.indexOf('Foo') >= 0).toBe(true);
   });
   it('can compile export (VariableDeclaration:const)', function() {
@@ -75,7 +75,7 @@ describe('compiler', function() {
     var s = c.compile('export const foo = "Foo";', 'sample.js', 'name.space');
     // console.log(s);
     expect(s.indexOf('goog.provide') >= 0).toBe(true);
-    expect(s.indexOf('name.space.foo') >= 0).toBe(true);
+    expect(s.indexOf('name.space.sample.foo') >= 0).toBe(true);
     expect(s.indexOf('Foo') >= 0).toBe(true);
   });
   it('can compile export (FunctionDeclaration)', function() {
@@ -83,7 +83,7 @@ describe('compiler', function() {
     var s = c.compile('export function f(a) { console.log(a); };', 'sample.js', 'name.space');
     // console.log(s);
     expect(s.indexOf('goog.provide') >= 0).toBe(true);
-    expect(s.indexOf('name.space.f') >= 0).toBe(true);
+    expect(s.indexOf('name.space.sample.f') >= 0).toBe(true);
     expect(s.indexOf('console.log') >= 0).toBe(true);
   });
   it('can compile export multi', function() {
@@ -91,7 +91,7 @@ describe('compiler', function() {
     var s = c.compile('export { foo, bar };', 'sample.js', 'name.space');
     // console.log(s);
     expect(s.indexOf('goog.provide') >= 0).toBe(true);
-    expect(s.indexOf('name.space.foo') >= 0).toBe(true);
+    expect(s.indexOf('name.space.sample.foo') >= 0).toBe(true);
     expect(s.indexOf('bar') >= 0).toBe(true);
   });
   it('can compile export A as B', function() {
@@ -99,7 +99,7 @@ describe('compiler', function() {
     var s = c.compile('export { foo as foofoo };', 'sample.js', 'name.space');
     // console.log(s);
     expect(s.indexOf('goog.provide') >= 0).toBe(true);
-    expect(s.indexOf('name.space.foofoo = foo') >= 0).toBe(true);
+    expect(s.indexOf('name.space.sample.foofoo = foo') >= 0).toBe(true);
   });
 
   // it('can compile default export', function() {
